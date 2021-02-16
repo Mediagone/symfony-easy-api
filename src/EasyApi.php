468f2b93645ce965f34d364ffa58f44ecf1f108c
Bruce <bruce@mediagone.fr>
2021-02-16 20:15:57 +0100

    Throw an error if callback doesn't return an ApiPayload instance

--- src/EasyApi.php
+++ src/EasyApi.php
@@ -2,7 +2,9 @@
 
 namespace Mediagone\Symfony\EasyApi;
 
+use LogicException;
 use Mediagone\Symfony\EasyApi\Errors\ApiBadRequestError;
+use Mediagone\Symfony\EasyApi\Payloads\ApiPayload;
 use Mediagone\Symfony\EasyApi\Payloads\ApiPayloadError400BadRequest;
 use Mediagone\Symfony\EasyApi\Payloads\ApiPayloadError500ServerError;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,6 +22,10 @@ final class EasyApi
     {
         try {
             $payload = $payloadGenerator();
+            
+            if (! $payload instanceof ApiPayload) {
+                throw new LogicException('The callback must return an ApiPayload instance.');
+            }
         }
         catch (ApiBadRequestError $ex) {
             $payload = ApiPayloadError400BadRequest::create($ex);
