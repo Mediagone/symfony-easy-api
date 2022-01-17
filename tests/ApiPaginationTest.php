1faacfc7b81e0e2a84e12614e582b0691ed73e17
Bruce <bruce@mediagone.fr>
2022-01-17 23:52:50 +0100

    Add "EasyApi" and "ApiPagination" tests

--- tests/ApiPaginationTest.php
+++ tests/ApiPaginationTest.php
@@ -3,14 +3,17 @@
 namespace Tests\Mediagone\Symfony\EasyApi;
 
 use InvalidArgumentException;
-use LogicException;
 use Mediagone\Symfony\EasyApi\EasyApi;
-use Mediagone\Symfony\EasyApi\Errors\ApiBadRequestError;
 use Mediagone\Symfony\EasyApi\Payloads\ApiPayload200Success;
+use Mediagone\Symfony\EasyApi\Payloads\ApiPayload201Created;
+use Mediagone\Symfony\EasyApi\Payloads\ApiPayload202Accepted;
 use Mediagone\Symfony\EasyApi\Request\ApiPagination;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use function json_encode;
+use function array_slice;
+use function ceil;
+use function count;
+use function json_decode;
 
 
 /**
@@ -21,19 +24,6 @@ final class ApiPaginationTest extends TestCase
     //========================================================================================================
     // 
     //========================================================================================================
-    
-    public function test_can_return_response() : void
-    {
-        $pagination = ApiPagination::create(2, 10, 33);
-        
-        self::assertSame(2, $pagination->getPage());
-        self::assertSame(10, $pagination->getItemsPerPage());
-        self::assertSame(33, $pagination->getItemsCountTotal());
-        
-        self::assertSame(4, $pagination->getPageCount());
-        self::assertSame(10, $pagination->getPageItemOffset());
-    }
-    
     
     public function test_throw_exception_on_invalid_page() : void
     {
@@ -56,6 +46,75 @@ final class ApiPaginationTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         
         ApiPagination::create(1, 10, -1);
+    }
+    
+    
+    
+    public function test_can_return_response() : void
+    {
+        $pagination = ApiPagination::create(2, 10, 33);
+        
+        self::assertSame(2, $pagination->getPage());
+        self::assertSame(10, $pagination->getItemsPerPage());
+        self::assertSame(33, $pagination->getItemsCountTotal());
+        
+        self::assertSame(4, $pagination->getPageCount());
+        self::assertSame(10, $pagination->getPageItemOffset());
+    }
+    
+    public function responseArrayResultProvider(): iterable
+    {
+        $objs = [
+            ['name' => 'Result 1'],
+            ['name' => 'Result 2'],
+            ['name' => 'Result 3'],
+        ];
+        $objsSubset = array_slice($objs, 0, 2);
+        $pagination = ApiPagination::create(1, count($objsSubset), count($objs));
+        
+        yield [$objs, $objsSubset, ApiPayload200Success::createWithArrayResult($objsSubset, $pagination), 200, 'ok'];
+        yield [$objs, $objsSubset, ApiPayload201Created::createWithArrayResult($objsSubset, $pagination), 201, 'created'];
+        yield [$objs, $objsSubset, ApiPayload202Accepted::createWithArrayResult($objsSubset, $pagination), 202, 'accepted'];
+    }
+    
+    /**
+     * @dataProvider responseArrayResultProvider
+     */
+    public function test_can_return_paginated_response($objs, $objsSubset, $responseObject, $responseCode, $responseMessage) : void
+    {
+        $easyApi = new EasyApi();
+        $response = $easyApi->response(function() use($responseObject) {
+            return $responseObject;
+        });
+        
+        self::assertInstanceOf(JsonResponse::class, $response);
+        self::assertSame($responseCode, $response->getStatusCode());
+        self::assertJson($response->getContent());
+        
+        $data = json_decode($response->getContent());
+        self::assertObjectHasAttribute('success', $data);
+        self::assertTrue($data->success);
+        self::assertObjectHasAttribute('status', $data);
+        self::assertSame($responseMessage, $data->status);
+        self::assertObjectHasAttribute('statusCode', $data);
+        self::assertSame($responseCode, $data->statusCode);
+        self::assertObjectHasAttribute('payload', $data);
+        self::assertIsObject($data->payload);
+        
+        self::assertObjectHasAttribute('results', $data->payload);
+        self::assertIsArray($data->payload->results);
+        self::assertObjectHasAttribute('resultsCount', $data->payload);
+        self::assertSame(count($objsSubset), $data->payload->resultsCount, 'result count');
+        self::assertObjectHasAttribute('resultsCountTotal', $data->payload);
+        self::assertSame(count($objs), $data->payload->resultsCountTotal);
+        self::assertObjectHasAttribute('page', $data->payload);
+        self::assertSame(1, $data->payload->page);
+        self::assertObjectHasAttribute('pageCount', $data->payload);
+        self::assertSame((int)ceil(count($objs) / count($objsSubset)), $data->payload->pageCount);
+        foreach ($objsSubset as $key => $item) {
+            self::assertObjectHasAttribute('name', $data->payload->results[$key]);
+            self::assertSame($item['name'], $data->payload->results[$key]->name);
+        }
     }
     
     
--- tests/EasyApiTest.php
+++ tests/EasyApiTest.php
@@ -6,9 +6,12 @@ use LogicException;
 use Mediagone\Symfony\EasyApi\EasyApi;
 use Mediagone\Symfony\EasyApi\Errors\ApiBadRequestError;
 use Mediagone\Symfony\EasyApi\Payloads\ApiPayload200Success;
+use Mediagone\Symfony\EasyApi\Payloads\ApiPayload201Created;
+use Mediagone\Symfony\EasyApi\Payloads\ApiPayload202Accepted;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use function json_encode;
+use function count;
+use function json_decode;
 
 
 /**
@@ -20,27 +23,139 @@ final class EasyApiTest extends TestCase
     // 
     //========================================================================================================
     
-    public function test_can_return_response() : void
+    public function responseProvider(): iterable
     {
-        $successResponse = ApiPayload200Success::create();
+        yield [ApiPayload200Success::create(), 200, 'ok'];
+        yield [ApiPayload201Created::create(), 201, 'created'];
+        yield [ApiPayload202Accepted::create(), 202, 'accepted'];
+    }
+    
+    /**
+     * @dataProvider responseProvider
+     */
+    public function test_can_return_a_payload($responseObject, $responseCode, $responseMessage) : void
+    {
+        // $responseObject = ApiPayload200Success::create();
+    
+        /*ApiResultCollection::fromArray([1,2,'aa'])*/
+        //$req = Request::create('http://localhost:8000/current-url?param=value');
+        //$paginator = ApiPagination::from(22, 10, 2);
         
         $easyApi = new EasyApi();
-        $response = $easyApi->response(function() use($successResponse) {
-            return $successResponse;
+        $response = $easyApi->response(function() use($responseObject) {
+            return $responseObject;
         });
         
         self::assertInstanceOf(JsonResponse::class, $response);
-        self::assertJson(json_encode($successResponse->getData(), JSON_THROW_ON_ERROR), $response->getContent());
-        self::assertSame(200, $response->getStatusCode());
+        self::assertSame($responseCode, $response->getStatusCode());
+        self::assertJson($response->getContent());
+    
+        $data = json_decode($response->getContent());
+        self::assertObjectHasAttribute('success', $data);
+        self::assertTrue($data->success);
+        self::assertObjectHasAttribute('status', $data);
+        self::assertSame($responseMessage, $data->status);
+        self::assertObjectHasAttribute('statusCode', $data);
+        self::assertSame($responseCode, $data->statusCode);
+        self::assertObjectHasAttribute('payload', $data);
+        self::assertNull($data->payload);
     }
     
     
-    public function test_generator_must_return_a_payload() : void
+    public function responseSingleResultProvider(): iterable
     {
-        $successResponse = ApiPayload200Success::create();
+        $obj = ['foo' => 'bar'];
         
+        yield [$obj, ApiPayload200Success::createWithSingleResult($obj), 200, 'ok'];
+        yield [$obj, ApiPayload201Created::createWithSingleResult($obj), 201, 'created'];
+        yield [$obj, ApiPayload202Accepted::createWithSingleResult($obj), 202, 'accepted'];
+    }
+    
+    /**
+     * @dataProvider responseSingleResultProvider
+     */
+    public function test_can_return_a_payload_with_single_result($obj, $responseObject, $responseCode, $responseMessage) : void
+    {
         $easyApi = new EasyApi();
-        $response = $easyApi->response(function() use($successResponse) {
+        $response = $easyApi->response(function() use($responseObject) {
+            return $responseObject;
+        });
+        self::assertInstanceOf(JsonResponse::class, $response);
+        self::assertSame($responseCode, $response->getStatusCode());
+        self::assertJson($response->getContent());
+        
+        $data = json_decode($response->getContent());
+        self::assertObjectHasAttribute('success', $data);
+        self::assertTrue($data->success);
+        self::assertObjectHasAttribute('status', $data);
+        self::assertSame($responseMessage, $data->status);
+        self::assertObjectHasAttribute('statusCode', $data);
+        self::assertSame($responseCode, $data->statusCode);
+        self::assertObjectHasAttribute('payload', $data);
+        self::assertIsObject($data->payload);
+        self::assertObjectHasAttribute('result', $data->payload);
+        self::assertIsObject($data->payload->result);
+        self::assertObjectHasAttribute('foo', $data->payload->result);
+        self::assertSame('bar', $data->payload->result->foo);
+    }
+    
+    
+    public function responseArrayResultProvider(): iterable
+    {
+        $objs = [
+            ['name' => 'Result 1'],
+            ['name' => 'Result 2'],
+        ];
+        
+        yield [$objs, ApiPayload200Success::createWithArrayResult($objs), 200, 'ok'];
+        yield [$objs, ApiPayload201Created::createWithArrayResult($objs), 201, 'created'];
+        yield [$objs, ApiPayload202Accepted::createWithArrayResult($objs), 202, 'accepted'];
+    }
+    
+    /**
+     * @dataProvider responseArrayResultProvider
+     */
+    public function test_can_return_a_payload_with_array_result($objs, $responseObject, $responseCode, $responseMessage) : void
+    {
+        $easyApi = new EasyApi();
+        $response = $easyApi->response(function() use($responseObject) {
+            return $responseObject;
+        });
+        self::assertInstanceOf(JsonResponse::class, $response);
+        self::assertSame($responseCode, $response->getStatusCode());
+        self::assertJson($response->getContent());
+        
+        $data = json_decode($response->getContent());
+        self::assertObjectHasAttribute('success', $data);
+        self::assertTrue($data->success);
+        self::assertObjectHasAttribute('status', $data);
+        self::assertSame($responseMessage, $data->status);
+        self::assertObjectHasAttribute('statusCode', $data);
+        self::assertSame($responseCode, $data->statusCode);
+        self::assertObjectHasAttribute('payload', $data);
+        self::assertIsObject($data->payload);
+        
+        self::assertObjectHasAttribute('results', $data->payload);
+        self::assertIsArray($data->payload->results);
+        self::assertObjectHasAttribute('resultsCount', $data->payload);
+        self::assertSame(count($objs), $data->payload->resultsCount);
+        self::assertObjectHasAttribute('resultsCountTotal', $data->payload);
+        self::assertSame(count($objs), $data->payload->resultsCountTotal);
+        self::assertObjectHasAttribute('page', $data->payload);
+        self::assertSame(1, $data->payload->page);
+        self::assertObjectHasAttribute('pageCount', $data->payload);
+        self::assertSame(1, $data->payload->pageCount);
+        foreach ($objs as $key => $item) {
+            self::assertObjectHasAttribute('name', $data->payload->results[$key]);
+            self::assertSame($item['name'], $data->payload->results[$key]->name);
+        }
+    }
+    
+    
+    public function test_generator_must_return_a_valid_payload() : void
+    {
+        $easyApi = new EasyApi();
+        $response = $easyApi->response(function() {
             return 'invalid payload';
         });
         
@@ -48,7 +163,7 @@ final class EasyApiTest extends TestCase
     }
     
     
-    public function test_can_handle_request_error() : void
+    public function test_can_handle_bad_request_error() : void
     {
         $easyApi = new EasyApi();
         $response = $easyApi->response(function() {
